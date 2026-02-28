39892c28d580e8a52df7009f8f044ada2541febf
shrsu <sujshrest@gmail.com>
2026-02-28 17:12:55 +0530

    debezium/dbz#1458 Create DevService and scripts for testing Debezium db2 Extension for Quarkus - Integration Tests
    Signed-off-by: shrsu <sujshrest@gmail.com>

--- debezium-quarkus-parent/debezium-quarkus-db2-parent/deployment/src/main/resources/db2-cdc-docker/cdcsetup.sh
+++ debezium-quarkus-parent/debezium-quarkus-db2-parent/deployment/src/main/resources/db2-cdc-docker/cdcsetup.sh
@@ -15,4 +15,16 @@ su  -c "/asncdctools/src/dbsetup.sh $DBNAME"   - db2inst1
 fi
 touch /asncdctools/src/asncdc.nlk
 
+# Wait for CDC infrastructure to be ready
+sleep 10
+
+# Run test table setup if in test mode
+if [ "$TEST_MODE" = "true" ]; then
+  echo "Setting up test tables..."
+  su - $DB2INSTANCE -s /bin/bash -c "db2 -tvmf /asncdctools/src/testsetup.sql"
+fi
+
+# Start capture agent AFTER tables have CDC enabled
+su - $DB2INSTANCE -s /bin/bash -c "nohup /database/config/$DB2INSTANCE/sqllib/bin/asncap capture_schema=asncdc capture_server=$DBNAME > /tmp/asncap.log 2>&1 &"
+
 echo "done"
