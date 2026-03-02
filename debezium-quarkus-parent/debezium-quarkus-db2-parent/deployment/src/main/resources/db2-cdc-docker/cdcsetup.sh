6ee4577278b60661c34f390b2125b4a6807de176
shrsu <sujshrest@gmail.com>
2026-03-02 11:21:56 +0530

    debezium/dbz#1458 Cleaned up testsetup.sql file
    Signed-off-by: shrsu <sujshrest@gmail.com>

--- debezium-quarkus-parent/debezium-quarkus-db2-parent/deployment/src/main/resources/db2-cdc-docker/cdcsetup.sh
+++ debezium-quarkus-parent/debezium-quarkus-db2-parent/deployment/src/main/resources/db2-cdc-docker/cdcsetup.sh
@@ -18,13 +18,7 @@ touch /asncdctools/src/asncdc.nlk
 # Wait for CDC infrastructure to be ready
 sleep 10
 
-# Run test table setup if in test mode
-if [ "$TEST_MODE" = "true" ]; then
-  echo "Setting up test tables..."
-  su - $DB2INSTANCE -s /bin/bash -c "db2 -tvmf /asncdctools/src/testsetup.sql"
-fi
-
-# Start capture agent AFTER tables have CDC enabled
+# Start capture agent
 su - $DB2INSTANCE -s /bin/bash -c "nohup /database/config/$DB2INSTANCE/sqllib/bin/asncap capture_schema=asncdc capture_server=$DBNAME > /tmp/asncap.log 2>&1 &"
 
 echo "CDC setup completed successfully"
